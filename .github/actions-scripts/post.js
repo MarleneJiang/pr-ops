26928009f66cc021462e0bb765407b1158a845bc
少轻狂 <1677568218@qq.com>
2023-07-19 01:26:57 +0800

    feat:11

--- .github/actions-scripts/post.js
+++ .github/actions-scripts/post.js
@@ -1,7 +1,7 @@
 // "{\"id\": \"${{ github.event.pull_request.number }}\", \"labels\": \"${{ steps.pr-labels.outputs.labels }}\", \"files_updated\": \"${{ steps.changed-files.outputs.files_updated }}\", \"files_created\": \"${{ steps.changed-files.outputs.files_created }}\", \"files_deleted\": \"${{ steps.changed-files.outputs.files_deleted }}\",\"body\":\"${{github.event.pull_request.body}}\",\"title\":\"${{github.event.pull_request.title}}\",\"url\":\"${{github.event.pull_request.html_url}}\"}"
 
 
-async function run() {
+function run() {
   // let { REPOSITORY: repository } = process.env
   let { ID: id, LABELS: labels, FILES_UPDATED: files_updated, FILES_CREATED: files_created, FILES_DELETED: files_deleted, BODY: body, TITLE: title, URL: url } = process.env
   // 如果labels的键不存在web或者security，那么就不执行下面的代码
@@ -12,7 +12,7 @@ async function run() {
 
   try {
     //post to dfggtr.baidu.com
-    await fetch('https://np4l5e.laf.run/pr', {
+    fetch('https://np4l5e.laf.run/pr', {
       method: 'POST',
       body: JSON.stringify({
         id,
@@ -27,6 +27,8 @@ async function run() {
       headers: {
         'Content-Type': 'application/json'
       }
+    }).then(()=>{
+      setOutput('post_result', '成功辣~');
     })
 
   } catch (e) {
@@ -34,8 +36,6 @@ async function run() {
 
           return;
   }
-
-  setOutput('post_result', '成功辣~');
 }
 
-await run();
+run();
